2bc2635e5854735e0dbbdcde04e93f3b0b575fe7
AlexandorOrlov <oalexandr248@gmail.com>
2024-08-16 19:00:53 +0300

    Spring Bean

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,27 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    public Dog dog() {
+        return new Dog();
+    }
+
+
+    @Bean
+    public Animal animal() {
+        return dog();  // Возвращаем Dog как Animal
+    }
+    @Bean
+    public Timer timer() {
+        return new Timer();
+    }
 }
